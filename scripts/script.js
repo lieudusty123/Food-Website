90f2841c16934b3f06d537c0a048da1193bc4393
Your Name <you@example.com>
2022-06-09 17:50:22 +0300

    visual updates, more filters

--- scripts/script.js
+++ scripts/script.js
@@ -74,7 +74,8 @@ inputContainer.addEventListener('submit', (e) => {
 })
 
 let nextPage
-// let APIdata = getAPIData().then(function (result) { return nextPage = result._links.next.href })
+// comment if needed
+let APIdata = getAPIData().then(function (result) { return nextPage = result._links.next.href })
 
 $(window).scroll(function () {
     if ($(window).scrollTop() + $(window).height() == $(document).height()) {
